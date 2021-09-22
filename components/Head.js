1182a81b9abe39b9a476e74e1a1e5a4d27d417d0
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2021-09-22 11:47:18 -0300

    Update Head.js

--- components/Head.js
+++ components/Head.js
@@ -8,6 +8,7 @@ function Cabecalho () {
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <meta name="author" content="Marcos Henrique Marcarini Junior" />
                 <title>Frigolima Supermercados</title>
+                <script data-ad-client="ca-pub-4774450614178988" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
             </Head>
         </div>
     )
