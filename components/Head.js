302bffd6d47c54e2f5d466ac9e5e3f9776332f2d
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2021-09-23 10:54:09 -0300

    Simple update
    Add google tag for analytics.

--- components/Head.js
+++ components/Head.js
@@ -9,6 +9,8 @@ function Cabecalho () {
                 <meta name="author" content="Marcos Henrique Marcarini Junior" />
                 <title>Frigolima Supermercados</title>
                 <script data-ad-client="ca-pub-4774450614178988" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-H0JSLG4FLH"></script>
+                <script src="/scripts.js"></script>
             </Head>
         </div>
     )
