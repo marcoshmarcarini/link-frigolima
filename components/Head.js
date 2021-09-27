f552725337644b071281e7a5a36f748c6b5028a3
marcoshmarcarini <40902689+marcoshmarcarini@users.noreply.github.com>
2021-09-27 10:47:25 -0300

    Update Head.js
    Adição de mais uma tag do Google Adsense.

--- components/Head.js
+++ components/Head.js
@@ -10,6 +10,7 @@ function Cabecalho () {
                 <meta name="author" content="Marcos Henrique Marcarini Junior" />
                 <title>Frigolima Supermercados</title>
                 <script data-ad-client="ca-pub-3480588623219549" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3480588623219549" crossorigin="anonymous"></script>
                 <Script async src="https://www.googletagmanager.com/gtag/js?id=G-H0JSLG4FLH"/>
                 <Script src="custom-scripts.js"/>
             </Head>
